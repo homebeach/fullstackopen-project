f34580ce66ec95b5ca295d21078c16a4e0b334c8
Petri Kotiranta <petri.kotiranta@exaum.com>
2024-12-19 23:36:52 +0200

    Migration improvements

--- migrations/20241218120001-create-users.js
+++ migrations/20241218120001-create-users.js
@@ -1,33 +1,54 @@
 'use strict';
 
-const { DataTypes, Sequelize } = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = {
   up: async ({ context: queryInterface }) => {
+    // Create the users table with all columns
     await queryInterface.createTable('users', {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
+        allowNull: false,
       },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
-      createdAt: {
-        type: DataTypes.DATE,
+      firstname: {
+        type: DataTypes.STRING,
         allowNull: false,
-        defaultValue: Sequelize.fn('NOW'),
       },
-      updatedAt: {
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      other_names: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      user_type: {
+        type: DataTypes.ENUM('customer', 'librarian', 'admin'),
+        allowNull: false,
+        defaultValue: 'customer',
+      },
+      created_at: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: Sequelize.fn('NOW'),
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
     });
   },
+
   down: async ({ context: queryInterface }) => {
+    // Drop the users table
     await queryInterface.dropTable('users');
   },
 };
